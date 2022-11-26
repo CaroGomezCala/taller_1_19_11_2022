4672de6e01d759149ea1fb7747b144b70923cd94
CaroGomezCala <lcgk.021@gmail.com>
2022-11-26 16:19:02 -0500

    solucion4

---
+++ todo_sobre_variables_19_11_2022/main.js
@@ -0,0 +1,14 @@
+addEventListener("DOMContentLoaded", (e)=>{
+    //declaramos las variables
+    let cadena = "Hola como estas?";
+    let numero = 25;
+    let boleano = true;
+    // imprimirmos por consola
+    console.log("Las variables son: ");
+    console.log(cadena);
+    console.log("el dato anterior es tipo: " + typeof(cadena));
+    console.log(numero);
+    console.log("el dato anterior es tipo: " + typeof(numero));
+    console.log(boleano);
+    console.log("el dato anterior es tipo: " + typeof(boleano));
+})
